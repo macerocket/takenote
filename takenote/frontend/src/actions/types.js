df02c41e44a9c2f4a0505d1de0ed732a59dda173
Macerocket <macerocket@yahoo.com>
2021-07-20 13:54:56 +0100

    Adjust message routing

--- takenote/frontend/src/actions/types.js
+++ takenote/frontend/src/actions/types.js
@@ -2,5 +2,4 @@ export const GET_NOTES = 'GET_NOTES';
 export const DELETE_NOTE = 'DELETE_NOTE';
 export const ADD_NOTE = 'ADD_NOTE';
 export const GET_ERRORS = 'GET_ERRORS';
-export const GET_MESSAGES = 'GET_MESSAGES';
 export const CREATE_MESSAGE = 'CREATE_MESSAGE';
